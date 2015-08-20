0619f49c19266d6ac46eadd937a5c1cf6689fccc
levenkov <artem@levenkov.org>
2015-08-20 14:43:44 +0300

    Investigations...

--- examples/example1.rb
+++ examples/example1.rb
@@ -1,29 +1,63 @@
-require 'pathname'
 require 'proxyfs'
+require 'stringio'
 require_relative 'debug'
 
+class Dir
+  def [] (name)
+    return nil unless entries.include? name
 
-class VirtualDir
+    res_path = File.join(path, name)
+    File.directory?(res_path) ? Dir.new(res_path) : File.new(res_path)
+  end
+
+  def directory?
+    true
+  end
+end
+
+class File
+  def directory?
+    false
+  end
+end
+
+class StringIO
+  def directory?
+    false
+  end
+end
+
+class ProxyDir
+  @data
+
   def initialize dir_data
+    raise ArgumentError, 'dir_data shall be Hash' unless Hash === dir_data
+
+    @data = dir_data
+  end
+
+  def [] (name)
+    res = @data[name]
+
+    res ? res[0].new(*res[1..(-1)]) : nil
+  end
+
+  def directory?
+    true
   end
 end
 
-class VirtualFile
-  def initialize file_data
-  end
-end
-
-root = [ VirtualDir, {
+$root = ProxyDir.new({
   'file1' => [ File, '/etc/fstab' ],
-  'file2' => [ VirtualFile, 'Hello, world!' ],
+  'file2' => [ StringIO, 'Hello, world!' ],
   'dir1' => [ Dir, '/etc' ],
-  'dir2' => [ VirtualDir, {
-    'file1' => [ VirtualFile, 'Blablabla' ],
+  'dir2' => [ ProxyDir, {
+    'file1' => [ StringIO, 'Blablabla' ],
   } ],
-} ]
+})
 
-MyDir = ProxyFS::dir root
-MyFile = ProxyFS::file root
+#MyDir = ProxyFS.dir root
+MyFile = ProxyFS.file $root
 
-BP[binding]
+#BP[binding]
 
--- lib/proxyfs.rb
+++ lib/proxyfs.rb
@@ -1,5 +1,10 @@
 module ProxyFS
-  def dir root
+  class VirtualDir
+    def initialize dir_data
+    end
+  end
+
+  def self.dir root
     res = Class.new
 
     res.class_eval do
@@ -32,10 +37,122 @@ module ProxyFS
         res
       end
     end
+
+    res
   end
 
-  def file root
+  def self.file root
     res = Class.new
+
+    res.class_eval do
+      @@root = root
+      @file
+      @path
+
+      def initialize path, mode="r"
+        f = self.class.entry_by_path path
+
+        raise ArgumentError, 'Path is a directory' if f.directory?
+
+        @file = f
+        @path = path
+      end
+
+      def self.entry_by_path path
+        parts = path.split '/'
+
+        raise ArgumentError, 'Relative path is not supported' unless parts[0].empty?
+        parts.delete_at 0
+
+        dir = @@root
+
+        basename = parts.pop
+
+        parts.each do |part|
+          tmp = dir[part]
+
+          return nil unless tmp && tmp.directory?
+
+          dir = tmp
+        end
+
+        dir[basename]
+      end
+
+      def self.define_proxy_method method_name
+        define_method method_name do |*args, &block|
+          @file.send method_name, *args, &block
+        end
+      end
+
+      def self.define_singleton_proxy_method method_name
+        define_singleton_method method_name do |*args, &block|
+          file = self.new(args[0])
+          file_class = file.file_class
+
+          if file.file_class.respond_to? method_name
+            file_class.send method_name, file.real_file_path, *args[1..(-1)], &block
+          else
+            file.send method_name, *args[1..(-1)]
+          end
+        end
+      end
+
+      [
+        :read, :write, :size, :atime, :mtime, :ctime,
+        :flock, :truncate, :chmod, :chown, :binmode,
+        :seek
+      ].each do |method|
+        define_proxy_method method
+      end
+
+      [
+        :read, :write, :size, :atime, :mtime, :ctime,
+        :binread, :binwrite
+      ].each do |method|
+        define_singleton_proxy_method method
+      end
+
+      def self.binread *args
+          file = self.new(args.shift)
+          file.binmode
+
+          if args.count == 2
+            file.seek args.pop
+          end
+
+          file.read *args
+      end
+
+      def self.binwrite *args
+          file = self.new(args.shift)
+          file.binmode
+
+          if args.count == 2
+            file.seek args.pop
+          end
+
+          file.write *args
+      end
+
+      def path
+        @path
+      end
+
+      def to_path
+        path
+      end
+
+      def file_class
+        @file.class
+      end
+
+      def real_file_path
+        @file.path
+      end
+    end
+
+    res
   end
 end
 
