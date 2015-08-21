4e992743849883594842ec283c6d2f4dc70bb2ee
levenkov <artem@levenkov.org>
2015-08-21 15:38:49 +0300

    Class operations added to ProxyFS::File.

--- lib/proxyfs.rb
+++ lib/proxyfs.rb
@@ -15,22 +15,57 @@ end
 module ProxyFS
   class File
     @path
+    @entry
+    @file
 
     def initialize path
       @path = path
+      @entry = fs.entry(@path)
+
+      if Array === @entry
+        @file = @entry[0].new(*@entry[1..(-1)])
+      elsif Hash === e
+      else
+        @file = @entry
+      end
     end
 
-    def read *args
-      e = fs.entry(@path)
+    define_proxy_method = Proc.new do |method, method_name|
+      self.send method, method_name do |*args, &block|
+        e = @entry.nil? ? fs.entry(args.shift) : @entry
 
-      if Array === e
-        f = e[0].new(*e[1..(-1)])
-        f.read *args
-      elsif Hash === e
-        raise ArgumentError, 'it is directory'
-      else
-        e.read *args
+        if Array === e
+          f = @file.nil? ? e[0].new(*e[1..(-1)]) : @file
+          f.send method_name, *args, &block
+        elsif Hash === e
+          raise ArgumentError, 'it is directory'
+        else
+          f = @entry.nil? ? e : @entry
+          f.send method_name, *args, &block
+        end
       end
+    end
+
+    def self.binread name, length=nil, offset=nil
+      f = self.new name
+      f.binmode
+      f.seek offset if offset
+      f.read *([length].compact)
+    end
+
+    def self.binwrite name, string, offset=nil
+      f = self.new name
+      f.binmode
+      f.seek offset if offset
+      f.write string
+    end
+
+    [:read, :write, :seek, :binmode].each do |name|
+      define_proxy_method[:define_method, name]
+    end
+
+    [:read, :write].each do |name|
+      define_proxy_method[:define_singleton_method, name]
     end
   end
 
